09992652233e5725ce182c2339d4ae8d30a4702f
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-13 17:19:58 +0200

    primo snack completo

--- js/snack1.js
+++ js/snack1.js
@@ -1,3 +1,20 @@
 console.log('JS OK');
 
 //* SNACK 1
+
+// array con gli invitati
+const guests = ['Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
+
+//array per la lista dei segnaposto
+const guestPlaceholders = [];
+
+for (let i = 0; i < guests.length; i++){
+    const guest = {
+        name: guests[i],
+        table: 'VIP table',
+        seat: i + 1
+    };
+    guestPlaceholders.push(guest);
+}
+// array con i segnaposto
+console.table(guestPlaceholders);
