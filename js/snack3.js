8a7a3e2fac6d65d2d62ece88959f3a433c2b9e88
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-06-14 16:19:25 +0200

    terzo snack completo e inzio quarto snack

--- js/snack3.js
+++ js/snack3.js
@@ -1,3 +1,58 @@
 console.log('JS OK');
 
 //* SNACK 3
+
+//* Informazioni prese da Google per creare l'array
+// Tra le bici da corsa italiane nominiamo alcuni marchi: Bianchi, Atala, Olmo, Casati, Colnago, Bottecchia, Cinelli, Pistidda.
+// In linea generale il peso di una bici da corsa dovrebbe rientrare nel minimo stabilito dall'UCI, fissato a 6,8 chili.
+
+//array delle bici
+const bikes = [
+    {
+        name: 'Bianchi',
+        weight: 6.8
+    },
+    {
+        name: 'Atala',
+        weight: 6.5
+    },
+    {
+        name: 'Olmo',
+        weight: 7.0
+    },
+    {
+        name: 'Casati',
+        weight: 6.7
+    },
+    {
+        name: 'Colnago',
+        weight: 6.9
+    },
+    {
+        name: 'Bottecchia',
+        weight: 6.6
+    },
+    {
+        name: 'Cinelli',
+        weight: 7.1
+    },
+    {
+        name: 'Pistidda',
+        weight: 6.4
+    },
+];
+console.table(bikes);
+
+//array del peso delle bici
+const bikesWeight = bikes.map(bike => bike.weight);
+console.log(bikesWeight);
+
+//peso della bici più leggera
+const lightBikeWeight = Math.min(...bikesWeight);
+console.log(lightBikeWeight);
+
+//array con solo la bici più leggera
+const lightBike = bikes.filter((bike) => {
+    return bike.weight === lightBikeWeight;
+})
+console.log(lightBike); //stampo nel log la bici più leggera
--- js/snack4.js
+++ js/snack4.js
@@ -1,3 +1,56 @@
 console.log('JS OK');
 
 //* SNACK 4
+
+//* Squadre di calcio prese da Google:
+// Juventus.
+// Inter.
+// Milan.
+// Roma.
+// Napoli.
+// Lazio.
+// Atalanta.
+// Fiorentina.
+
+const footballTeams = [
+    {
+        name: 'Juventus',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Inter',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Milan',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Roma',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Napoli',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Lazio',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Atalanta',
+        points: 0,
+        foul: 0
+    },
+    {
+        name: 'Fiorentina',
+        points: 0,
+        foul: 0
+    },
+]
